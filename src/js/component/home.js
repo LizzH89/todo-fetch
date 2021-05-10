8bbca4c8d7bb4c8bb7de3c2133864e140de94f35
LizzH89 <lizziehidalgoleiva@gmail.com>
2021-05-10 16:50:31 +0000

    final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,135 @@
-import React from "react";
+import React, { useState, useEffect, Component } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const InputToDo = () => {
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([{ label: "", done: false }]);
 
-//create your first component
-export function Home() {
+	const handleChange = event => {
+		setTask(event.target.value);
+	};
+
+	const getList = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Lhidalgo89", {
+			method: "GET",
+			headers: {
+				"content-type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.then(response => {
+				setList(
+					response.map((item, index) => {
+						return item; //el map siempre debe llevar un                          return
+					})
+				);
+			});
+	};
+
+	const createList = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Lhidalgo89", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: []
+		})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+
+	const updateList = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Lhidalgo89", {
+			method: "PUT",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: JSON.stringify(list)
+		})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+
+	const deleteList = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Lhidalgo89", {
+			method: "DELETE",
+			headers: {
+				"content-type": "application/json"
+			}
+		})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+
+	const handleSubmit = event => {
+		if (task.trim() && task.length !== 0) {
+			setList(list.concat({ label: task, done: false }));
+		} else {
+			alert("Task can not be empty");
+		}
+		console.log(list);
+		setTask("");
+		event.preventDefault();
+	};
+
+	const removeTodo = index => {
+		const newTodos = [...list];
+		newTodos.splice(index, 1);
+		setList(newTodos);
+	};
+	updateList();
+
+	useEffect(() => {
+		getList();
+	}, []);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="title">
+				To Do List
+				<i className="fas fa-tasks" />
+			</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					className="d-inline-block align-middle divInput"
+					type="text"
+					value={task}
+					onChange={handleChange}
+					placeholder="What´s next to be done?"
+				/>
+				<button className="btn btn-primary addTask" type="submit">
+					Add Task
+				</button>
+			</form>
+			<ul className="list-group">
+				{list.map((item, index) => (
+					<li className="list-group-item d-flex" key={index}>
+						{item.label}
+						<i
+							onClick={removeTodo}
+							className="far fa-trash-alt ml-auto"
+						/>
+					</li>
+				))}
+				<div className="taskCounter">
+					You have{" "}
+					<strong className="length">
+						{list.length} tasks to do{" "}
+					</strong>
+				</div>
+			</ul>
 		</div>
 	);
-}
+};
+export default InputToDo;
